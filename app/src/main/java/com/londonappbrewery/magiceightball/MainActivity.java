b4d5bcd55aa308d5940dde932f48f82ed8154a68
Kumar <34918826+kumar-spy@users.noreply.github.com>
2018-06-29 03:14:17 +0530

    message alignment problem and hand size small

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button askbutton;
+        askbutton=(Button)findViewById(R.id.askbutton);
+        final ImageView message=(ImageView)findViewById(R.id.msg_1);
+        final int[] msgarray={R.drawable.msg1,
+                R.drawable.msg2,
+                R.drawable.msg3,
+                R.drawable.msg4,
+                R.drawable.msg5,
+                R.drawable.msg6,};
+        askbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator=new Random();
+                int number=randomNumberGenerator.nextInt(6);
+                message.setImageResource(msgarray[number]);
+
+            }
+        });
+
+
     }
 }
